60f9152250d6a73aea56c0bb98e040a81aaa0ac2
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-03-26 22:05:20 +0100

    feat: add communication between servers

--- README.md
+++ README.md
@@ -16,11 +16,15 @@ go test
 
 ### Run
 ```
-./server "localhost:8000"
+./server localhost:8001 localhost:8002 localhost:8003 localhost:8004 localhost:8005
+./server localhost:8002 localhost:8001 localhost:8003 localhost:8004 localhost:8005
+./server localhost:8003 localhost:8001 localhost:8002 localhost:8004 localhost:8005
+./server localhost:8004 localhost:8001 localhost:8002 localhost:8003 localhost:8005
+./server localhost:8005 localhost:8001 localhost:8002 localhost:8003 localhost:8004
 ```
 
 ```
-./client "localhost:8000"
+./client localhost:8001 localhost:8002 localhost:8003 localhost:8004 localhost:8005
 ```
 
 License
--- protocol/clinet.go
+++ protocol/clinet.go
@@ -2,22 +2,14 @@ package protocol
 
 import (
 	"log"
-	"net/rpc"
 )
 
-var connectionsServers map[string]Server = make(map[string]Server)
 var idCurrentServer string
-
-type Server struct {
-	url        string
-	connection *rpc.Client
-}
 
 type RpcClient int
 
 func (t *RpcClient) Send(value int, reply *bool) error {
-	*reply = true
-	log.Printf("Received: %d", value)
+	*reply = handleClientRequest(value)
 	return nil
 }
 
@@ -27,17 +19,4 @@ func Send(value int) {
 	if err != nil {
 		log.Println("Unable to send: ", err)
 	}
-}
-
-func Connect(url string) bool {
-	log.Println("Connecting to server: " + url)
-	connection, err := rpc.Dial("tcp", url)
-	connectionsServers[url] = Server{url: url, connection: connection}
-	if err != nil {
-		log.Println("Unable Connecting to server: ", err)
-		return false
-	}
-	idCurrentServer = url
-	log.Println("Connected to server: " + url)
-	return true
 }
