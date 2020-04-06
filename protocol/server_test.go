06807af16f9b81665e5624672615b9c0d945c30d
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-04-06 12:03:22 +0200

    test: test vote request for the same term

--- protocol/server_test.go
+++ protocol/server_test.go
@@ -10,6 +10,24 @@ func TestRequestVoteLowerTerm(t *testing.T) {
 	}
 }
 
+func TestRequestVoteSameTermNotVotedYet(t *testing.T) {
+	currentTerm = 3
+	votedFor = vote{}
+	_, isVoted := requestVote(3, "localhost:8000", 2, 2)
+	if !isVoted {
+		t.Errorf("requestVote() is %t", isVoted)
+	}
+}
+
+func TestRequestVoteSameTermAlreadyVoted(t *testing.T) {
+	currentTerm = 3
+	votedFor = vote{"localhost:8000", 3}
+	_, isVoted := requestVote(3, "localhost:8000", 2, 2)
+	if isVoted {
+		t.Errorf("requestVote() is %t", isVoted)
+	}
+}
+
 func TestAppendEntriesLowerTerm(t *testing.T) {
 	currentTerm = 3
 	_, isVoted := appendEntries(2, "localhost:8000", 2, 2, make([]LogEntry, 0), 2)
@@ -18,6 +36,5 @@ func TestAppendEntriesLowerTerm(t *testing.T) {
 	}
 }
 
-// TODO don't vote for an already voted term
 // TODO AppendEntries heartbeat
 // TODO nextIndex[] and matchIndex[] is reset after leader election
